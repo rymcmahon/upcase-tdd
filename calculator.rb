0572a111cbb596f06337bf593ae8b1623fbb9cd8
rymcmahon <toedragdangle9@gmail.com>
2018-04-04 18:56:20 -0400

    finished calculator exercise

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,15 @@ require "rspec/autorun"
 
 class Calculator
   def add(a, b)
-    15
+    a + b
+  end
+
+  def factorial(n)
+    if n == 0
+      1
+    else
+      (1..n).reduce(:*)
+    end
   end
 end
 
@@ -13,5 +21,26 @@ describe Calculator do
 
       expect(calc.add(5, 10)).to eq(15)
     end
+
+    it "returns the sum of two different arguments" do
+      calc = Calculator.new
+
+      expect(calc.add(1, 2)).to eq(3)
+
+    end
+  end
+end
+
+describe "#factorial" do
+  it "returns 1 when given 0" do
+    calc = Calculator.new
+
+    expect(calc.factorial(0)).to eq(1)
+  end
+
+  it "returns 120 when given 5" do
+    calc = Calculator.new
+
+    expect(calc.factorial(5)).to eq(120)
   end
 end
